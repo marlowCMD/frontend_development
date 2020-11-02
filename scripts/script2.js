6fd7bedf42e8309aee5f40c3ea4d25aa50fef5ea
marlowCMD <63289726+marlowCMD@users.noreply.github.com>
2020-11-02 17:10:12 +0100

    Add files via upload

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,34 +1,31 @@
 
-var carouselLinksButton = document.querySelector(".carouselbutton2left")
-var carouselRechtsButton = document.querySelector(".carouselbutton2right")
-var thumbnail = document.querySelector("#screenshots > img")
-var screenShot = document.querySelector("screenshot1")
-var screenShotTwo = document.querySelector("screenshot2")
-var screenShotThree = document.querySelector("screenshot3")
-var arrayThumbs = ['thumb1.jpg', 'thumb2.jpg', 'thumb3.jpg']
+var carouselLinksButton = document.querySelector(".carouselbutton2left");
+var carouselRechtsButton = document.querySelector(".carouselbutton2right");
+var thumbnail = document.querySelector("#screenshots > img");
+var arrayThumbs = ['big1.jpg', 'big2.jpg', 'big3.jpg'];
 
-var currentThumb = 0
-var lastThumb = arrayThumbs.length - 1
-thumbnail.src = 'images/' + arrayThumbs[currentThumb]
+var currentThumb = 0;
+var lastThumb = arrayThumbs.length - 1;
+thumbnail.src = 'images/' + arrayThumbs[currentThumb];
 
 function nextThumb () {
     if (currentThumb < lastThumb) {
-        console.log(currentThumb)
+        console.log(currentThumb);
         currentThumb++;
-    thumbnail.src = 'images/' + arrayThumbs[currentThumb]
+    thumbnail.src = 'images/' + arrayThumbs[currentThumb];
     } else {
-    currentThumb = 0
-    thumbnail.src = 'images/' + arrayThumbs[currentThumb]
+    currentThumb = 0;
+    thumbnail.src = 'images/' + arrayThumbs[currentThumb];
     }
 }
 
 function prevThumb () {
     if (currentThumb > 0) {
         currentThumb--;
-        thumbnail.src = 'images/' + arrayThumbs[currentThumb]
+        thumbnail.src = 'images/' + arrayThumbs[currentThumb];
     } else {
         currentThumb = lastThumb;
-    thumbnail.src = 'images/' + arrayThumbs[currentThumb]
+    thumbnail.src = 'images/' + arrayThumbs[currentThumb];
     }
 }
 
